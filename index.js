06d8f640aef461ff8729f30d3db80b9ce685fac6
Jayson <czljcb@163.com>
2018-01-02 23:20:26 +0800

    nav

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 // import App from './ztc/main/index';
-import App from './demo/Test';
+// import App from './demo/Test';
+import App from './component/index';
 
 AppRegistry.registerComponent('ztc', () => App);
