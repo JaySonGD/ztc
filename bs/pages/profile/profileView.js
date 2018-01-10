c7c72bac144337d4059ee52808d8056b8be17584
Ward Wong <ward@WarddeMac-mini.local>
2018-01-10 17:29:33 +0800

    imagepicker

--- bs/pages/profile/profileView.js
+++ bs/pages/profile/profileView.js
@@ -9,18 +9,72 @@ import {
     View,
     Text,
     StyleSheet,
+    Image,
+    Button,
 } from 'react-native';
 
+import ImagePicker from 'react-native-image-picker';
+
+const options = {
+    title: 'Select Avatar',
+    customButtons: [
+        {name: 'fb', title: 'Choose Photo from Facebook'},
+    ],
+    storageOptions: {
+        skipBackup: true,
+        path: 'images'
+    }
+};
+
 export default class ProfileView extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            avatarSource : null,
+        }
+    }
 
     render(){
 
         return(
             <View style={styles.container}>
                 <Text>ProfileView</Text>
+                {this.state.avatarSource? <Image source={this.state.avatarSource} style={{width:100,height:100}}/> : null}
+                <Button title="上传" onPress={()=>{
+                    this._selectImage()
+                }}/>
             </View>
         )
 
+
+
+    }
+
+    _selectImage(){
+        ImagePicker.showImagePicker(options, (response) => {
+            console.log('Response = ', response);
+
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            }
+            else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            }
+            else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            }
+            else {
+                let source = { uri: response.uri };
+
+                // You can also display the image using data:
+                // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+                this.setState({
+                    avatarSource: source
+                })
+            }
+        })
     }
 }
 
